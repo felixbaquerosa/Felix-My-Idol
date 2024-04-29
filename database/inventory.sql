09f3118cf876d1d3e2616a3a46c364b1a2d43824
Daiplatinue <Scheme307@gmail.com>
2024-04-29 23:34:46 +0800

    asd

--- database/inventory.sql
+++ database/inventory.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 14, 2024 at 08:51 PM
+-- Generation Time: Apr 29, 2024 at 05:31 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -31,8 +31,8 @@ CREATE TABLE `inventory` (
   `id` int(50) NOT NULL,
   `email` varchar(50) NOT NULL,
   `contact` varchar(50) NOT NULL,
-  `user` varchar(50) NOT NULL,
-  `pass` varchar(50) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(250) NOT NULL,
   `type` varchar(50) NOT NULL,
   `status` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -41,9 +41,11 @@ CREATE TABLE `inventory` (
 -- Dumping data for table `inventory`
 --
 
-INSERT INTO `inventory` (`id`, `email`, `contact`, `user`, `pass`, `type`, `status`) VALUES
-(4, 'sirMikepahong@gmail.com', '09123546879', 'aisdjailsdjiaosld', 'Asd123!@#', 'ADMIN', 'DECLINED'),
-(8, '12312312312', '3122132132312332113123', 'tangina', 'tangina', 'USER', 'ACTIVE');
+INSERT INTO `inventory` (`id`, `email`, `contact`, `username`, `password`, `type`, `status`) VALUES
+(13, '1', '1', 'USERNAME', 'MZ9NJuPFNrXdhxuyxS4xeA==', 'ADMIN', 'DECLINED'),
+(14, 'email', '1', 'user', 'EBGT1xgcyINArlsrF7uooQ==', 'Admin', 'ACTIVE'),
+(15, '2', '2', '2', 'nfYjWBcP90UXiA7Mgn1V1Q==', 'USER', 'ACTIVE'),
+(16, '3', '3', '3', '9bsMjeFGxntEurv05lhMwA==', 'USER', 'ACTIVE');
 
 --
 -- Indexes for dumped tables
@@ -63,7 +65,7 @@ ALTER TABLE `inventory`
 -- AUTO_INCREMENT for table `inventory`
 --
 ALTER TABLE `inventory`
-  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
